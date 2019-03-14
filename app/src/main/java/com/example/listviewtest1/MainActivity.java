439cde00c3e7b7a86810062aa02186e8b97dc101
minjun <a01073119317@icloud.com>
2019-03-14 23:42:51 +0900

    ListView completion

--- app/src/main/java/com/example/listviewtest1/MainActivity.java
+++ app/src/main/java/com/example/listviewtest1/MainActivity.java
@@ -18,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listview = (ListView) findViewById(R.id.ListView);
 
+        SingerAdapter adapter = new SingerAdapter(); // 객체 //어댑터 객체 설정
+        adapter.addItem(new SingerItem("소녀시대", "010-1000-1000"));
+        adapter.addItem(new SingerItem("빅뱅", "010-2000-2000"));
+        adapter.addItem(new SingerItem("승리", "010-3000-3000"));
+        adapter.addItem(new SingerItem("태양", "010-4000-4000"));
+        adapter.addItem(new SingerItem("대성", "010-5000-5000"));
+        adapter.addItem(new SingerItem("쥐디", "010-6000-6000"));
+        adapter.addItem(new SingerItem("탑", "010-7000-7000"));
+        adapter.addItem(new SingerItem("걸스데이", "010-8000-8000"));
 
+        listview.setAdapter(adapter);
 
     }
 
@@ -29,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return items.size();
+        }
+
+        public void addItem(SingerItem item) {
+            items.add(item);
         }
 
         @Override
@@ -45,8 +59,11 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             SingerItemView view = new SingerItemView(getApplicationContext());
 
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            view.setMobile(item.getMoblie());
 
-            return null;
+            return view;
         }
     }
 
